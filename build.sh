34aa56e4b56b8815d2e23bca7ee4409f081eb290
fboulnois <fboulnois@users.noreply.github.com>
2022-11-07 14:14:13 -0500

    feat: add standard tests for all options

--- build.sh
+++ build.sh
@@ -29,10 +29,27 @@ run() {
         "$CWD" "$@"
 }
 
+tests() {
+    mkdir -p output
+    docker run --rm --gpus=all \
+        -v huggingface:/home/huggingface/.cache/huggingface \
+        -v "$PWD"/output:/home/huggingface/output \
+        "$CWD" "abstract art"
+    docker run --rm --gpus=all \
+        -v huggingface:/home/huggingface/.cache/huggingface \
+        -v "$PWD"/output:/home/huggingface/output \
+        "$CWD" --model "runwayml/stable-diffusion-v1-5" \
+            --H 512 --W 512 --n_samples 2 --n_iter 2 --seed 42 \
+            --scale 7.5 --ddim_steps 80 --attention-slicing \
+            --half --skip --negative-prompt "red roses" \
+            --prompt "bouquet of roses"
+}
+
 case ${1:-build} in
     build) build ;;
     clean) clean ;;
     dev) dev "$@" ;;
     run) run "$@" ;;
+    test) tests ;;
     *) echo "$0: No command named '$1'" ;;
 esac
