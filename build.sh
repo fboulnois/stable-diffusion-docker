25e2c75c99a638eed0acf6ccbcef4fd530fe4191
fboulnois <fboulnois@users.noreply.github.com>
2022-12-12 14:48:20 -0500

    feat: add test for image upscaling

--- build.sh
+++ build.sh
@@ -45,6 +45,9 @@ tests() {
     cp "img/${TEST_IMAGE}" "input/${TEST_IMAGE}"
     run --H 512 --W 640 "abstract art"
     run --device cpu --image "${TEST_IMAGE}" --strength 0.6 "abstract art"
+    run --model "stabilityai/stable-diffusion-x4-upscaler" \
+        --image "${TEST_IMAGE}" --half --attention-slicing \
+        --prompt "An impressionist painting of a parakeet eating spaghetti in the desert"
     run --model "runwayml/stable-diffusion-v1-5" \
         --n_samples 2 --n_iter 2 --seed 42 \
         --scale 7.5 --ddim_steps 80 --attention-slicing \
